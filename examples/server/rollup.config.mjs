43e93c02c894134d8c243c47c5ed6c2751f3468a
ROG <luan.roger.2003@gmail.com>
2025-08-16 18:09:10 -0300

    feat: update example

---
+++ examples/server/rollup.config.mjs
@@ -0,0 +1,33 @@
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+  input: "src/main.ts",
+  output: {
+    dir: "dist",
+    format: "cjs",
+    sourcemap: true,
+    exports: "auto",
+  },
+  treeshake: {
+    moduleSideEffects: false,
+    propertyReadSideEffects: false,
+  },
+  plugins: [
+    resolve({
+      preferBuiltins: true,
+      browser: false,
+    }),
+
+    commonjs({
+      include: /node_modules/,
+    }),
+
+    typescript({
+      tsconfig: "./tsconfig.json",
+      sourceMap: true,
+      inlineSources: true,
+    }),
+  ],
+};
--- examples/server/src/main.ts
+++ examples/server/src/main.ts
@@ -1,3 +1,8 @@
-import { Hono } from "only";
+import { Hono } from "only/server";
+import { serve } from "only/server/node";
 
 const app = new Hono();
+
+app.get("/", (c) => c.text("Hello World!"));
+
+serve(app);
