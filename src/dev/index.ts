477d687bbb46d7a5442db257f66ebae67ed0e99b
ROG <luan.roger.2003@gmail.com>
2025-08-17 16:28:07 -0300

    feat: create default Biome and TS config in JSON

---
+++ src/dev/index.ts
@@ -0,0 +1,5 @@
+export * as mod from "./vite";
+export * from "./vite";
+export { default as vite } from "./vite";
+export { default as biome } from "./biome";
+export { default as tsconfig } from "./ts-config";
