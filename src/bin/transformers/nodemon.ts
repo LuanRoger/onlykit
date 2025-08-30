7e3edbb22699f206bc9b9ade0bdb3609383a6068
ROG <luan.roger.2003@gmail.com>
2025-08-30 15:12:37 -0300

    fix(bin): remove colors from nodemon output

--- src/bin/transformers/nodemon.ts
+++ src/bin/transformers/nodemon.ts
@@ -1,8 +1,16 @@
 import { NodemonProcessMetadata } from "../processes";
+import { clearAnsiLessLine } from "../utils/formater";
 import { mountProcessPrefix } from "../utils/log";
 
 function isNodemonLine(line: string): boolean {
-  return line.startsWith("[nodemon]");
+  if (typeof line !== "string") {
+    return false;
+  }
+
+  const trimmedLine = line.trim();
+  const clearedLine = clearAnsiLessLine(trimmedLine);
+  const prefix = clearedLine.slice(0, 9);
+  return prefix === "[nodemon]";
 }
 
 function removeNodemonPrefix(line: string): string {
@@ -11,20 +19,17 @@ function removeNodemonPrefix(line: string): string {
 
 function clearLine(line: string): string {
   const withoutPrefix = removeNodemonPrefix(line);
+  const clearedLine = clearAnsiLessLine(withoutPrefix);
 
-  return withoutPrefix;
+  return clearedLine;
 }
 
 let lineCounter = 0;
 // biome-ignore lint/suspicious/noExplicitAny: Any to match the transformer function signature
 export function* transformNodemon(line: any) {
   if (lineCounter >= 3) {
-    if (!isNodemonLine(line)) {
-      yield line;
-    }
-
     const runnerPrefix = mountProcessPrefix(NodemonProcessMetadata.instance);
-    const clearedLine = clearLine(line);
+    const clearedLine = isNodemonLine(line) ? clearLine(line) : line;
     const finalLine = `${runnerPrefix}${clearedLine}`;
 
     console.log(finalLine);
