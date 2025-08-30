195b4c67682acce144b787fbfbb4e7169b170e67
ROG <luan.roger.2003@gmail.com>
2025-08-30 17:56:45 -0300

    feat(server): export Hono jsx, quick and tiny

---
+++ src/server/quick/index.ts
@@ -0,0 +1,5 @@
+import * as mod from "hono/quick";
+
+export * from "hono/quick";
+export { mod as serverQuick };
+export default mod;
---
+++ src/server/tiny/index.ts
@@ -0,0 +1,5 @@
+import * as mod from "hono/tiny";
+
+export * from "hono/tiny";
+export { mod as serverTiny };
+export default mod;
